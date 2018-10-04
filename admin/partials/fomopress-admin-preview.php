093a21072d4daf3ae0a3e8eda45c8da923e95fdb
Priyo Mukul <priyomukul@Priyos-MacBook-Pro.local>
2018-10-04 13:31:57 +0600

    Admin Previe for notification is ready, but not interactive yet

--- admin/partials/fomopress-admin-preview.php
+++ admin/partials/fomopress-admin-preview.php
@@ -1,3 +1,13 @@
 <div class="fomopress-notification-preview">
-    <h1>Hello World</h1>
+    <div class="fomopress-preview-inner">
+        <div class="fomopress-preview-image">
+            <img src="<?php echo FOMOPRESS_ADMIN_URL . 'assets/img/placeholder-300x300.png'; ?>" alt="">
+        </div>
+        <div class="fomopress-preview-content">
+            <span class="fomopress-preview-row fomopress-preview-first-row fomopress-highlight"><?php _e( 'John D. recently purchased', 'fomopress' ); ?></span>
+            <span class="fomopress-preview-row fomopress-preview-second-row"><?php _e( 'Example Product', 'fomopress' ); ?></span>
+            <span class="fomopress-preview-row fomopress-preview-third-row"><?php _e( '1 hour ago', 'fomopress' ); ?></span>
+        </div>
+        <span class="fomopress-preview-close">x</span>
+    </div>
 </div>
