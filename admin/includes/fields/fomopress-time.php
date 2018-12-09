f0b41ec9df832d5da887e0cfb7f2bc7ccce321a2
Priyo Mukul <git@priyo.me>
2018-12-09 13:11:51 +0600

    Countdown Time Control Updated

--- admin/includes/fields/fomopress-time.php
+++ admin/includes/fields/fomopress-time.php
@@ -14,25 +14,25 @@
 <div class="fomopress-countdown-inputs">
     <div class="fomopress-countdown-input fomopress-days">
         <input 
-            placeholder="Days" step="1" min="0" max="31" 
+            placeholder="Days" min="0" max="31" 
             id="<?php echo $name; ?>[days]" 
             type="number" name="<?php echo $name; ?>[days]" value="<?php echo $value['days']; ?>">
     </div>
     <div class="fomopress-countdown-input fomopress-hours">
         <input 
-            placeholder="Hours" step="1" min="0" max="23" 
+            placeholder="Hours" min="0" max="23" 
             id="<?php echo $name; ?>[hours]" 
             type="number" name="<?php echo $name; ?>[hours]" value="<?php echo $value['hours']; ?>">
     </div>
     <div class="fomopress-countdown-input fomopress-minutes">
         <input 
-            placeholder="Minutes" step="5" min="0" max="59" 
+            placeholder="Minutes" min="0" max="59" 
             id="<?php echo $name; ?>[minutes]" 
             type="number" name="<?php echo $name; ?>[minutes]" value="<?php echo $value['minutes']; ?>">
         </div>
     <div class="fomopress-countdown-input fomopress-seconds">
         <input 
-            placeholder="Seconds" step="10" min="0" max="59" 
+            placeholder="Seconds" min="0" max="59" 
             id="<?php echo $name; ?>[seconds]" 
             type="number" name="<?php echo $name; ?>[seconds]" value="<?php echo $value['seconds']; ?>">
     </div>
