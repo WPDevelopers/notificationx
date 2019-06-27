dc0b1f9c6725b4faf2856db1fcc8b6b861353fca
Priyo Mukul <priyomukul@gmail.com>
2019-06-27 12:17:13 +0600

    NX_CONSTANTS Added

---
+++ includes/class-nx-const.php
@@ -0,0 +1,39 @@
+<?php 
+class NX_CONSTANTS {
+    public static $COMMENT_SECTIONS = array(
+        'comment_themes', 
+        'comment_design',
+        'comment_image_design',
+        'comment_typography'
+    );
+    public static $COMMENT_FIELDS = array(
+        'comments_source', 
+        'comment_advance_edit', 
+        'comments_template_new',
+        'comments_template_new_adv',
+        'comments_template',
+        'link_options',
+        'show_avatar',
+    );
+
+    public static $CONVERSION_SECTIONS = array(
+        'themes', 
+        'design',
+        'image_design',
+        'typography'
+    );
+    public static $CONVERSION_FIELDS = array(
+        'conversion_from', 
+        'advance_edit', 
+        'woo_template_new',
+        'woo_template_new_adv',
+        'woo_template',
+        'product_control',
+        'product_list',
+        'category_list',
+        'product_exclude_by',
+        'exclude_categories',
+        'exclude_products',
+        'conversion_url',
+    );
+}
