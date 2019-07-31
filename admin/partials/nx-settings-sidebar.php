a0bf8fb3bbc7735446a431cb85b7de605f942676
Priyo Mukul <priyomukul@gmail.com>
2019-07-31 14:51:43 +0600

    Icon Added

--- admin/partials/nx-settings-sidebar.php
+++ admin/partials/nx-settings-sidebar.php
@@ -27,7 +27,7 @@
         <div class="nx-admin-block nx-admin-block-docs">
             <header class="nx-admin-block-header">
                 <div class="nx-admin-block-header-icon">
-                    <img src="https://eae.dev/wp-content/plugins/essential-addons-for-elementor-lite/assets/admin/images/icon-documentation.svg" alt="essential-addons-for-elementor-documentation">
+                    <img src="<?php echo NOTIFICATIONX_ADMIN_URL; ?>/assets/img/icons/icon-documentation.svg" alt="notificationx-documentation">
                 </div>
                 <h4 class="nx-admin-title"><?php _e( 'Documentation', 'notificationx' ); ?></h4>
             </header>
@@ -36,22 +36,22 @@
                 <a href="https://notificationx.com/docs/" class="nx-button" target="_blank"><?php _e( 'Documentation', 'notificationx' ); ?></a>
             </div>
         </div>
-        <div class="nx-admin-block nx-admin-block-docs">
+        <div class="nx-admin-block nx-admin-block-contribute">
             <header class="nx-admin-block-header">
                 <div class="nx-admin-block-header-icon">
-                    <img src="https://eae.dev/wp-content/plugins/essential-addons-for-elementor-lite/assets/admin/images/icon-documentation.svg" alt="essential-addons-for-elementor-documentation">
+                    <img src="<?php echo NOTIFICATIONX_ADMIN_URL; ?>/assets/img/icons/icon-contribute.svg" alt="notificationx-contribute">
                 </div>
-                <h4 class="nx-admin-title"><?php _e( 'Contribute to Essential Addons', 'notificationx' ); ?></h4>
+                <h4 class="nx-admin-title"><?php _e( 'Contribute to NotificationX', 'notificationx' ); ?></h4>
             </header>
             <div class="nx-admin-block-content">
-                <p><?php echo sprintf( '%1$s <a href="%2$s">Github</a>.', __( 'You can contribute to make Essential Addons better reporting bugs, creating issues, pull requests at', 'notificationx' ), 'https://github.com/WPDevelopers/notificationx' ); ?></p>
+                <p><?php echo sprintf( '%1$s <a href="%2$s">Github</a>.', __( 'You can contribute to make NotificationX better reporting bugs, creating issues, pull requests at', 'notificationx' ), 'https://github.com/WPDevelopers/notificationx' ); ?></p>
                 <a href="https://github.com/WPDevelopers/notificationx/issues/new" class="nx-button" target="_blank"><?php _e( 'Report a bug', 'notificationx' ); ?></a>
             </div>
         </div>
-        <div class="nx-admin-block nx-admin-block-docs">
+        <div class="nx-admin-block nx-admin-block-need-help">
             <header class="nx-admin-block-header">
                 <div class="nx-admin-block-header-icon">
-                    <img src="https://eae.dev/wp-content/plugins/essential-addons-for-elementor-lite/assets/admin/images/icon-documentation.svg" alt="essential-addons-for-elementor-documentation">
+                    <img src="<?php echo NOTIFICATIONX_ADMIN_URL; ?>/assets/img/icons/icon-need-help.svg" alt="notificationx-help">
                 </div>
                 <h4 class="nx-admin-title"><?php _e( 'Need Help?', 'notificationx' ); ?></h4>
             </header>
@@ -60,10 +60,10 @@
                 <a href="https://wpdeveloper.net" class="nx-button" target="_blank"><?php _e( 'Initiate a Chat', 'notificationx' ); ?></a>
             </div>
         </div>
-        <div class="nx-admin-block nx-admin-block-docs">
+        <div class="nx-admin-block nx-admin-block-community">
             <header class="nx-admin-block-header">
                 <div class="nx-admin-block-header-icon">
-                    <img src="https://eae.dev/wp-content/plugins/essential-addons-for-elementor-lite/assets/admin/images/icon-documentation.svg" alt="essential-addons-for-elementor-documentation">
+                    <img src="<?php echo NOTIFICATIONX_ADMIN_URL; ?>/assets/img/icons/icon-join-community.svg" alt="notificationx-commuinity">
                 </div>
                 <h4 class="nx-admin-title"><?php _e( 'Join the Community', 'notificationx' ); ?></h4>
             </header>
