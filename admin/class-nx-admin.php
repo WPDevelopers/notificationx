13316532ecd908f9316bf6c9d26b100e2233fb2c
Priyo Mukul <priyomukul@gmail.com>
2019-08-05 14:30:29 +0600

    version updated to v1.2.1 and image optimized

--- admin/class-nx-admin.php
+++ admin/class-nx-admin.php
@@ -478,6 +478,10 @@ class NotificationX_Admin {
 		) );
 
 		add_menu_page( 'NotificationX', 'NotificationX', 'delete_users', 'nx-admin', array( $this, 'notificationx' ), NOTIFICATIONX_ADMIN_URL . 'assets/img/nx-menu-icon.png', 80 );
+		/**
+		 * @since 1.2.1
+		 */
+		add_submenu_page( 'nx-admin', __('Add New', 'notificationx'), __('Add New', 'notificationx'), 'delete_users', 'post-new.php?post_type=notificationx');
 		foreach( $settings as $slug => $setting ) {
 			$cap  = isset( $setting['capability'] ) ? $setting['capability'] : 'delete_users';
 			$hook = add_submenu_page( 'nx-admin', $setting['title'], $setting['title'], $cap, $slug, $setting['callback'] );
