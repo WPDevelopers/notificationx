13a642578dd898b7c339cbc32ed343d4e42b9619
Rahat89 <rahat.feni99@gmail.com>
2019-08-22 13:06:28 +0600

    refactor nx_action_links

--- admin/class-nx-admin.php
+++ admin/class-nx-admin.php
@@ -909,14 +909,11 @@ class NotificationX_Admin {
      */
     public function nx_action_links($links)
     {
-        $deactivate_link = $links['deactivate'];
-        unset($links['deactivate']);
-        $links['settings'] = '<a href="' . admin_url('admin.php?page=nx-settings') . '">' . __('Settings','notificationx') .'</a>';
         if(!is_plugin_active('notificationx-pro/notificationx-pro.php')){
             $links['pro'] = '<a href="' . esc_url('http://wpdeveloper.net/in/upgrade-notificationx') . '" target="_blank" style="color: #349e34;"><b>' . __('Go pro','notificationx') .'</b></a>';
         }
-        $links['deactivate'] = $deactivate_link;
-        return $links;
+        $links['settings'] = '<a href="' . admin_url('admin.php?page=nx-settings') . '">' . __('Settings','notificationx') .'</a>';
+        return array_reverse($links);
     }
 
     /**
