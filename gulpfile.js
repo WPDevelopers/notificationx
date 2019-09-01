1bc9aff58842ad70326febbad57b1fbd9ca5c983
Rahat89 <rahat.feni99@gmail.com>
2019-09-01 16:38:48 +0600

    converted public css to scss

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,7 @@
 var gulp = require('gulp'),
 	sass = require('gulp-sass'),
 	cleanCss = require('gulp-clean-css'),
+	concat = require('gulp-concat'),
 	sourcemaps = require('gulp-sourcemaps');
 var paths = {
 	pAssets: 'public/assets/',
@@ -13,11 +14,19 @@ gulp.task('pSass', function(){
 	return gulp.src( paths.pStyles.src )
 		.pipe(sourcemaps.init())
 		.pipe( sass())
+		.pipe(cleanCss({format: 'beautify',level: 0}))
+		.pipe(sourcemaps.write(''))
+		.pipe( gulp.dest( paths.pStyles.dest ));
+});
+gulp.task('pConcat',function () {
+	return gulp.src(paths.pAssets + 'css/notificationx-public.css')
+		.pipe(sourcemaps.init())
+		.pipe(concat('notificationx-public.min.css'))
 		.pipe(cleanCss())
 		.pipe(sourcemaps.write(''))
-		.pipe( gulp.dest(paths.pStyles.dest ));
+		.pipe( gulp.dest( paths.pStyles.dest ));
 });
-
 gulp.task('watch', function() {
 	gulp.watch( paths.pAssets + 'scss/**/*.scss',  gulp.series('pSass'));
+	gulp.watch( paths.pAssets + 'css/notificationx-public.css',  gulp.series('pConcat'));
 });
