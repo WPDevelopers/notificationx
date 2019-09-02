0ffc8ef63c0b15afb7d413f6354ba8e2cfcc0473
Priyo Mukul <priyomukul@gmail.com>
2019-09-02 13:38:01 +0600

    SELECT field optgroup support

--- admin/includes/fields/nx-select.php
+++ admin/includes/fields/nx-select.php
@@ -14,12 +14,25 @@
 <select class="<?php echo esc_attr( $class ); ?>" <?php echo $multiple; ?> name="<?php echo $name; ?>" id="<?php echo $field_id; ?>" <?php echo $attrs; ?>>
     <?php 
         foreach( $field['options'] as $opt_id => $option ) {
-            if( is_array( $value ) ) {
-                $selected = in_array( $opt_id, $value ) ? 'selected="true"' : '';
+            if( is_array( $option ) ) {
+                echo '<optgroup label="'. $option['label'] .'">';
+                foreach( $option['options'] as $i_opt_id => $i_option ) {
+                    if( is_array( $value ) ) {
+                        $selected = in_array( $i_opt_id, $value ) ? 'selected="true"' : '';
+                    } else {
+                        $selected = ( $value == $i_opt_id ) ? 'selected="true"' : '';
+                    }
+                    echo '<option value="'. $i_opt_id .'" '. $selected .'>'. $i_option .'</option>';
+                }
+                echo '</optgroup>';
             } else {
-                $selected = ( $value == $opt_id ) ? 'selected="true"' : '';
+                if( is_array( $value ) ) {
+                    $selected = in_array( $opt_id, $value ) ? 'selected="true"' : '';
+                } else {
+                    $selected = ( $value == $opt_id ) ? 'selected="true"' : '';
+                }
+                echo '<option value="'. $opt_id .'" '. $selected .'>'. $option .'</option>';
             }
-            echo '<option value="'. $opt_id .'" '. $selected .'>'. $option .'</option>';
         }
     ?>
 </select>
