d0c0397f9737fafe3d00e2e7630ce47a86ca00b8
Priyo Mukul <priyomukul@gmail.com>
2019-09-05 15:31:43 +0600

    COMMENTS advanced themes

--- public/includes/class-nx-advanced-style.php
+++ public/includes/class-nx-advanced-style.php
@@ -20,6 +20,12 @@ class NotificationX_Advanced_Style {
             if( $settings->display_type === 'press_bar' ) {
                 $css_string = self::press_bar_edit( $settings, $theme );
             }
+            if( $settings->display_type === 'conversions' ) {
+                $css_string = self::conversions_edit( $settings, $theme );
+            }
+            if( $settings->display_type === 'comments' ) {
+                $css_string = self::comments_edit( $settings, $theme );
+            }
         }
 
         if( ! empty( $css_string ) ) {
@@ -31,7 +37,165 @@ class NotificationX_Advanced_Style {
 
         return $css_string;
     }
+    public static function comments_edit( $settings, $theme = 'theme-one' ){
+        $css_object = [];
+        $css_string = '';
 
+        if( ! empty( $settings->comment_bg_color ) ) {
+            $css_object[ 'wrapper' ][] = 'background-color:' . $settings->comment_bg_color;
+            $css_object['shadow']['color'] = $settings->comment_bg_color;
+        }
+        if( ! empty( $settings->comment_text_color ) ) {
+            $css_object[ 'wrapper' ][] = 'color:' . $settings->comment_text_color;
+            $css_object[ 'color' ][] = 'color:' . $settings->comment_text_color;
+        }
+        if( $settings->comment_border ) {
+            if( ! empty( $settings->comment_border_size ) ) {
+                $css_object[ 'border' ][] = 'border-width:' . $settings->comment_border_size . 'px';
+                if( ! empty( $settings->comment_border_style ) ) {
+                    $css_object[ 'border' ][] = 'border-style:' . $settings->comment_border_style;
+                }
+                if( ! empty( $settings->comment_border_color ) ) {
+                    $css_object[ 'border' ][] = 'border-color:' . $settings->comment_border_color;
+                    $css_object[ 'shadow' ]['border-color'] = $settings->comment_border_color;
+                }
+            }
+        }
+
+        if( ! empty( $settings->comment_first_font_size ) ) {
+            $css_object[ 'first_row' ][] = 'font-size:' . $settings->comment_first_font_size . 'px';
+        }
+        if( ! empty( $settings->comment_second_font_size ) ) {
+            $css_object[ 'second_row' ][] = 'font-size:' . $settings->comment_second_font_size . 'px';
+        }
+        if( ! empty( $settings->comment_third_font_size ) ) {
+            $css_object[ 'second_row' ][] = 'font-size:' . $settings->comment_third_font_size . 'px';
+        }
+
+        $custom_class = '.nx-notification.nx-' . $settings->display_type . ' .nx-customize-style-' . $settings->id;
+        $inner_class = $custom_class . '.nx-notification-' . $theme . '.notificationx-inner';
+        $content_class = $inner_class . ' .notificationx-content';
+        $image_class = $inner_class . ' .notificationx-image';
+
+        if( ! empty( $css_object['wrapper'] ) ) {
+            $wrapper_css = $inner_class . '{' . implode( ';', $css_object['wrapper'] ) . '}';
+            // THEME SIX
+            if( $theme === 'theme-six-free' ) {
+                $wrapper_css = $content_class . '{' . implode( ';', $css_object['wrapper'] ) . '}';
+                $wrapper_css .= $image_class . '{' . implode( ';', $css_object['wrapper'] ) . '}';
+                $wrapper_css .= $image_class . ':after{border-right-color: '. $css_object['shadow']['color'] .' }';
+            }
+            $css_string .= $wrapper_css;
+        }
+        if( ! empty( $css_object['border'] ) ) {
+            $border_css = $inner_class . '{' . implode( ';', $css_object['border'] ) . '}';
+            if( $theme === 'theme-six-free' ) {
+                $border_css = $content_class . '{' . implode( ';', $css_object['border'] ) . '}';
+                $border_css .= $image_class . '{' . implode( ';', $css_object['border'] ) . '}';
+            }
+            $css_string .= $border_css;
+        }
+        if( ! empty( $css_object['first_row'] ) ) {
+            $css_string .= $content_class . ' .nx-first-row{' . implode( ';', $css_object['first_row'] ) . '}';
+        }
+        if( ! empty( $css_object['second_row'] ) ) {
+            $css_string .= $content_class . ' .nx-second-row{' . implode( ';', $css_object['second_row'] ) . '}';
+        }
+        if( ! empty( $css_object['third_row'] ) ) {
+            $css_string .= $content_class . ' .nx-third-row{' . implode( ';', $css_object['third_row'] ) . '}';
+        }
+        if( ! empty( $css_object[ 'color' ] ) ) {
+            $css_string .= $content_class . ' > div {' . implode( ';', $css_object['color'] ) . '}';
+            $css_string .= $content_class . ' > div > span {' . implode( ';', $css_object['color'] ) . '}';
+        }
+
+        return $css_string;
+    }
+    public static function conversions_edit( $settings, $theme = 'theme-one' ){
+        $css_object = [];
+        $css_string = '';
+
+        if( ! empty( $settings->bg_color ) ) {
+            $css_object[ 'wrapper' ][] = 'background-color:' . $settings->bg_color;
+            $css_object['shadow']['color'] = $settings->bg_color;
+        }
+        if( ! empty( $settings->text_color ) ) {
+            $css_object[ 'wrapper' ][] = 'color:' . $settings->text_color;
+            $css_object[ 'color' ][] = 'color:' . $settings->text_color;
+        }
+        if( $settings->border ) {
+            if( ! empty( $settings->border_size ) ) {
+                $css_object[ 'border' ][] = 'border-width:' . $settings->border_size . 'px';
+                if( ! empty( $settings->border_style ) ) {
+                    $css_object[ 'border' ][] = 'border-style:' . $settings->border_style;
+                }
+                if( ! empty( $settings->border_color ) ) {
+                    $css_object[ 'border' ][] = 'border-color:' . $settings->border_color;
+                    $css_object[ 'shadow' ]['border-color'] = $settings->border_color;
+                }
+            }
+        }
+
+        if( ! empty( $settings->first_font_size ) ) {
+            $css_object[ 'first_row' ][] = 'font-size:' . $settings->first_font_size . 'px';
+        }
+        if( ! empty( $settings->second_font_size ) ) {
+            $css_object[ 'second_row' ][] = 'font-size:' . $settings->second_font_size . 'px';
+        }
+        if( ! empty( $settings->third_font_size ) ) {
+            $css_object[ 'second_row' ][] = 'font-size:' . $settings->third_font_size . 'px';
+        }
+
+        $custom_class = '.nx-notification.nx-' . $settings->display_type . ' .nx-customize-style-' . $settings->id;
+        $inner_class = $custom_class . '.nx-notification-' . $theme . '.notificationx-inner';
+        $content_class = $inner_class . ' .notificationx-content';
+        $image_class = $inner_class . ' .notificationx-image';
+
+        if( ! empty( $css_object['wrapper'] ) ) {
+            $wrapper_css = $inner_class . '{' . implode( ';', $css_object['wrapper'] ) . '}';
+            if( $theme === 'theme-four' ) {
+                $wrapper_css = $content_class . '{' . implode( ';', $css_object['wrapper'] ) . '}';
+                if( ! empty( $css_object['shadow']['color'] ) ) {
+                    $box_shadow = '0 0 0px 10px ' . $css_object['shadow']['color'];
+                }
+                if( ! empty( $css_object['shadow']['border-color'] ) ) {
+                    $box_shadow .= ',0 0 0px 11px ' . $css_object['shadow']['border-color'];
+                }
+                $box_shadow .= ',-10px 0px 30px -20px #b3b3b3';
+
+                $wrapper_css .= $image_class . '{box-shadow: '. $box_shadow .'}';
+            }
+            $css_string .= $wrapper_css;
+        }
+        if( ! empty( $css_object['border'] ) ) {
+            $border_css = $inner_class . '{' . implode( ';', $css_object['border'] ) . '}';
+            if( $theme === 'theme-four' ) {
+                $border_css = $content_class . '{' . implode( ';', $css_object['border'] ) . '}';
+            }
+            $css_string .= $border_css;
+        }
+        if( ! empty( $css_object['first_row'] ) ) {
+            $css_string .= $content_class . ' .nx-first-row{' . implode( ';', $css_object['first_row'] ) . '}';
+        }
+        if( ! empty( $css_object['second_row'] ) ) {
+            $css_string .= $content_class . ' .nx-second-row{' . implode( ';', $css_object['second_row'] ) . '}';
+        }
+        if( ! empty( $css_object['third_row'] ) ) {
+            $css_string .= $content_class . ' .nx-third-row{' . implode( ';', $css_object['third_row'] ) . '}';
+        }
+        if( ! empty( $css_object[ 'color' ] ) ) {
+            $css_string .= $content_class . ' > div {' . implode( ';', $css_object['color'] ) . '}';
+        }
+
+        return $css_string;
+    }
+    /**
+     * Press Bar Advance CSS
+     *
+     * @param stdClass $settings
+     * @param string $theme
+     * @return string
+     */
     public static function press_bar_edit( $settings, $theme = 'theme-one' ){
         $css_object = [];
         $css_string = '';
