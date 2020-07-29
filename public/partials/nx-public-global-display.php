935d57437a9f58334c1b7f7be39f02295aaf6112
Priyo Mukul <priyomukul@gmail.com>
2020-07-29 15:14:43 +0600

    Improved Display From

--- public/partials/nx-public-global-display.php
+++ public/partials/nx-public-global-display.php
@@ -4,11 +4,11 @@ if( ! is_array( $all_data ) ) {
 }
 
 $last_count = [];
-
 foreach( $all_data as $value ) {
     $settings = $value['settings'];
     unset( $value['settings'] );
     $type = $extension_name = NotificationX_Helper::get_type( $settings );
+    $from = strtotime( '-' . intval( $settings->display_from ) . ' days');
     if( 'conversions' === $type ) {
         $key = $settings->conversion_from;
         $extension_name = $key;
