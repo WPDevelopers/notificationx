3038a061d6c371aa4b2710bfb861af10308d5c1c
Priyo Mukul <priyomukul@gmail.com>
2021-05-19 10:58:44 +0600

    Fixed: PHP 8.0.* Conflict

--- admin/partials/nx-settings-form.php
+++ admin/partials/nx-settings-form.php
@@ -1,6 +1,6 @@
 <div class="nx-settings-form-wrapper">
     <form method="post" id="nx-settings-form" action="#">
-    <?php 
+    <?php
         $i = 1;
         /**
          * Settings Tab Content Rendering
@@ -11,32 +11,32 @@
             $sections = NotificationX_Helper::sorter( $sections, 'priority', 'ASC' );
             $is_form = isset( $setting['form'] ) ? $setting['form'] : false;
         ?>
-        <div 
-            id="nx-<?php echo esc_attr( $tab_key ); ?>" 
+        <div
+            id="nx-<?php echo esc_attr( $tab_key ); ?>"
             class="nx-settings-tab nx-settings-<?php echo esc_attr( $key );?> <?php echo $active; ?>">
-            <?php 
+            <?php
                     if( isset( $setting['views'] ) && ! empty( $setting['views'] ) ) {
                         call_user_func_array( $setting['views'], isset( $setting['sections'] ) ? array( 'sections' => $setting['sections'] ) : [] );
                     }
                     if( ! empty( $sections ) && ! isset( $setting['views'] ) ) :
                         /**
-                         * Every Section of a tab 
+                         * Every Section of a tab
                          * Rendering.
                          */
                         foreach( $sections as $sec_key => $section ) :
                             $fields = isset( $section['fields'] ) ? $section['fields'] : [];
                             $fields = NotificationX_Helper::sorter( $fields, 'priority', 'ASC' );
-                            ?>                                 
-                            <div 
-                                id="nx-meta-section-<?php echo esc_attr( $sec_key ); ?>" 
+                            ?>
+                            <div
+                                id="nx-meta-section-<?php echo esc_attr( $sec_key ); ?>"
                                 class="nx-settings-section nx-<?php echo esc_attr( $sec_key ); ?>">
-                                <?php 
+                                <?php
                                 if( isset( $section['title'] ) ) {
                                     echo '<h2 data-text="'. $section['title'] .'">'. $section['title'] .'</h2>';
                                 }
                                 if( isset( $section['views'] ) && ! empty( $section['views'] ) ) {
-                                    call_user_func_array( $section['views'], 
-                                        isset( $section['fields'] ) ? array( 'fields' => $section['fields'] ) : [] );
+                                    call_user_func_array( $section['views'],
+                                        isset( $section['fields'] ) ? [ $section['fields'] ] : [] );
                                 }
                                 /**
                                  * Every Section Field Rendering
@@ -44,7 +44,7 @@
                                 if( ! empty( $fields ) && ! isset( $section['views'] ) ) : ?>
                                 <table>
                                     <tbody>
-                                    <?php 
+                                    <?php
                                         foreach( $fields as $field_key => $field ) :
                                             NotificationX_Settings::render_field( $field_key, $field );
                                         endforeach;
