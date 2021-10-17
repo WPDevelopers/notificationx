8f650aeb405e840ad739ccd08d5d8e6dd602cfac
Md. Alimuzzaman Alim <alim@ar.com.bd>
2021-10-17 16:28:15 +0600

    Update index.tsx

--- nxdev/index.tsx
+++ nxdev/index.tsx
@@ -8,12 +8,6 @@ import apiFetch from "@wordpress/api-fetch";
 import 'quickbuilder/dist/index.css';
 
 (function () {
-    if(notificationxTabs?.rest?.root){
-        apiFetch.use(apiFetch.createNonceMiddleware(notificationxTabs.rest.nonce));
-        apiFetch.use(apiFetch.createRootURLMiddleware(notificationxTabs.rest.root));
-        // apiFetch.nonceEndpoint = admin_url( 'admin-ajax.php?action=rest-nonce' )
-        // wp_default_packages_inline_scripts
-    }
 
     addFilter('wprf_tab_content', 'NotificationX', (x, props) => {
         return !props.is_pro_active && props.current_page === 'settings' && <Sidebar assetsUrl={props.assets} is_pro_active={props.is_pro_active} />
