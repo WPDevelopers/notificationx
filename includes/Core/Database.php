ff1498b685cebdc045b3f3f40ad984855a566712
Md. Alimuzzaman Alim <alim@ar.com.bd>
2021-11-14 17:29:34 +0600

    Update Database.php

--- includes/Core/Database.php
+++ includes/Core/Database.php
@@ -31,7 +31,6 @@ class Database {
     public static $table_entries;
     public static $table_posts;
     public static $table_stats;
-    public static $table_maps;
 
     /**
      * Initially Invoked when initialized.
@@ -42,7 +41,6 @@ class Database {
         self::$table_entries   = $wpdb->prefix . 'nx_entries';
         self::$table_posts     = $wpdb->prefix . 'nx_posts';
         self::$table_stats     = $wpdb->prefix . 'nx_stats';
-        self::$table_maps      = $wpdb->prefix . 'nx_maps';
     }
 
 
@@ -51,7 +49,6 @@ class Database {
         $table_posts      = self::$table_posts;
         $table_entries    = self::$table_entries;
         $table_stats      = self::$table_stats;
-        $table_maps       = self::$table_maps;
 
         $sql = "CREATE TABLE {$table_entries} (
                 entry_id bigint(20) unsigned NOT NULL auto_increment,
@@ -98,36 +95,6 @@ class Database {
             ) $charset_collate ;";
         $stats_db = dbDelta($sql);
 
-        $sql = "CREATE TABLE {$table_maps} (
-                map_id bigint(20) unsigned NOT NULL auto_increment,
-                ip varchar(55) default NULL,
-                lat varchar(55) default 0,
-                lon varchar(55) default 0,
-                data longtext,
-                created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-                PRIMARY KEY (map_id),
-                KEY ip (ip)
-            ) $charset_collate ;";
-        $stats_db = dbDelta($sql);
-    }
-
-    public function insert_map($map_data){
-        $table_name = self::$table_maps;
-        if(!empty($map_data['data'])){
-            $map_data['data'] = maybe_serialize($map_data['data']);
-        }
-        return $this->wpdb->insert($table_name, $map_data);
-    }
-
-    public function get_map($where){
-        $table_name = self::$table_maps;
-        $sql = "SELECT data from $table_name ";
-        $sql .= $this->get_where_query($where);
-        $results = $this->wpdb->get_row($sql, ARRAY_A);
-        if(!empty($results['data'])){
-            return maybe_unserialize($results['data']);
-        }
-        return [];
     }
 
     public function update_analytics($type, $id, $date, $data = null) {
