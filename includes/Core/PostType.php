80a24db1151b1a6c3099eaa9ddcbe01db6d4a4c2
Md. Alimuzzaman Alim <alim@ar.com.bd>
2022-04-12 15:51:07 +0600

    Update PostType.php

--- includes/Core/PostType.php
+++ includes/Core/PostType.php
@@ -86,6 +86,10 @@ class PostType {
         wp_set_script_translations( 'notificationx-admin', 'notificationx' );
         do_action( 'notificationx_admin_scripts' );
 
+        // removing emoji support
+        remove_action( 'wp_head', 'print_emoji_detection_script', 7 );
+        remove_action( 'admin_print_scripts', 'print_emoji_detection_script' );
+
     }
 
     public function get_localize_scripts() {
