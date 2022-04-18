377646346545f08991635f24ffdb77004ad4351f
Md. Alimuzzaman Alim <alim@ar.com.bd>
2022-04-18 12:25:13 +0600

    Update Inline.php

--- includes/Types/Inline.php
+++ includes/Types/Inline.php
@@ -38,9 +38,9 @@ class Inline extends Types {
             "confirmButtonText" => __("<a href='https://notificationx.com/#pricing' target='_blank'>Upgrade to PRO</a>", "notificationx"),
             "html"=> __('
                 <span>Highlight your sales, low stock updates with inline growth alert to boost sales</span>
-                <!--<iframe id="email_subscription_video" type="text/html" allowfullscreen width="450" height="235"
-                src="https://www.youtube.com/embed/OuTmDZ0_TEw">
-                </iframe>-->
+                <iframe id="email_subscription_video" type="text/html" allowfullscreen width="450" height="235"
+                src="https://www.youtube.com/embed/vXMtBPvizDw">
+                </iframe>
             ', 'notificationx')
         ];
 
