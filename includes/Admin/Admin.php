81a19688446f97414c4b7fb601f46426b15e8e93
Md. Alimuzzaman Alim <alim@wpdeveloper.com>
2022-11-21 18:53:17 +0600

    Update Admin.php

--- includes/Admin/Admin.php
+++ includes/Admin/Admin.php
@@ -206,10 +206,28 @@ class Admin {
             [ $this->insights, 'notice' ],
             [
                 'classes'     => 'updated put-dismiss-notice',
-                'start'       => $notices->time(),
+                'start'       => $notices->strtotime( '+30 days' ),
                 'dismissible' => true,
                 'do_action'   => 'wpdeveloper_notice_clicked_for_notificationx',
                 'display_if'  => ! is_array( $notices->is_installed( 'notificationx-pro/notificationx-pro.php' ) )
+            ]
+        );
+
+        $notice_text = sprintf('%s <a href="%s">%s</a>', __( 'Black Friday Exclusive: SAVE up to 40% & access to <strong>NotificationX Pro</strong> features.', 'notificationx' ), esc_url( 'https://notificationx.com/#pricing' ), __('Grab The Offer', 'notificationx') );
+
+        $_black_friday = [
+            'thumbnail' => self::ASSET_URL . 'images/nx-icon.svg',
+            'html' => $notice_text,
+        ];
+
+        $notices->add(
+            'black_friday',
+            $_black_friday,
+            [
+                'start'       => $notices->time(),
+                'recurrence'  => false,
+                'dismissible' => true,
+                'expire'      => strtotime( 'Wed, 30 Nov 2022 23:59:59 GMT' ),
             ]
         );
 
