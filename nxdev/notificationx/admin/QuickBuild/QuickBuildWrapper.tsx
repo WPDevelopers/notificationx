ca840df845e7a29d2ca4eef1a7574776f6881339
sapanmozammel <sapanmozammel@gmail.com>
2023-01-05 16:29:53 +0600

    react modal create

--- nxdev/notificationx/admin/QuickBuild/QuickBuildWrapper.tsx
+++ nxdev/notificationx/admin/QuickBuild/QuickBuildWrapper.tsx
@@ -1,8 +1,5 @@
 import React, { useEffect, useState } from "react";
-import {
-    BuilderProvider,
-    useBuilder,
-} from "quickbuilder";
+import { BuilderProvider, useBuilder } from "quickbuilder";
 import { Header, WrapperWithLoader } from "../../components";
 import QuickBuild from "./QuickBuild";
 // @ts-ignore
@@ -10,8 +7,10 @@ import { __experimentalGetSettings, date } from "@wordpress/date";
 import moment from "moment";
 import withDocumentTitle from "../../core/withDocumentTitle";
 import { sprintf, __ } from "@wordpress/i18n";
+import Modal from "../../fields/Modal";
 
 const QuickBuildWrapper = (props) => {
+    const [isOpen, setIsOpen] = useState(true);
     const builder = useBuilder(notificationxTabs.quick_build);
     const [isLoading, setIsLoading] = useState(true);
     const [title, setTitle] = useState("");
@@ -24,19 +23,31 @@ const QuickBuildWrapper = (props) => {
     useEffect(() => {
         const type = builder?.values?.type;
         const title = builder.types_title?.[type];
-        const _value = moment.utc().utcOffset(+settings?.timezone?.offset);  //
+        const _value = moment.utc().utcOffset(+settings?.timezone?.offset); //
         const _date = date(settings.formats.date, _value, undefined);
         // translators: Title of the new Notification Alert. %1$s: title, %2$s: current date.
-        setTitle(sprintf(__("NotificationX - %1$s - %2$s", 'notificationx'), title, _date))
-    }, [builder?.values?.type])
+        setTitle(
+            sprintf(
+                __("NotificationX - %1$s - %2$s", "notificationx"),
+                title,
+                _date
+            )
+        );
+    }, [builder?.values?.type]);
 
     return (
-        <BuilderProvider value={{ ...builder, isLoading, setIsLoading, title, setTitle }}>
+        <BuilderProvider
+            value={{ ...builder, isLoading, setIsLoading, title, setTitle }}
+        >
             <Header addNew={true} />
             <WrapperWithLoader isLoading={isLoading}>
                 <QuickBuild isLoading={isLoading} />
             </WrapperWithLoader>
+            <Modal isOpen={isOpen} setIsOpen={setIsOpen} />
         </BuilderProvider>
     );
 };
-export default withDocumentTitle(QuickBuildWrapper, __("Quick Builder", 'notificationx'));
+export default withDocumentTitle(
+    QuickBuildWrapper,
+    __("Quick Builder", "notificationx")
+);
