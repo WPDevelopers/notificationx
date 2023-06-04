2784112a6600d910e22db60fe2bc2640c374df45
Md. Alimuzzaman Alim <alimuzzamanalim@gmail.com>
2023-06-04 13:10:50 +0600

    Flashing Tab

--- nxdev/notificationx/frontend/flashing-tab.ts
+++ nxdev/notificationx/frontend/flashing-tab.ts
@@ -1,69 +1,98 @@
+// Import the favloader and interval modules
 import favloader from "./flashing/favloader";
 import interval from "./flashing/webWorker";
 
-let interval_id = null;
+// Declare constants for the settings and messages
 // @ts-ignore
-const settings     = window.nx_flashing_tab;
-const ogTitle      = window.document.title;
-const initialDelay = 2000; // @todo get from settings
-const delayBetween = 2000; // @todo get from settings
+const settings = window.nx_flashing_tab;
+const initialDelay = 100;
+const delayBetween = 1000;
+const message1 = {
+ icon : 'https://nxm.test/wp-content/plugins/notificationx/assets/public/image/flashing-tab/theme-3-icon-1.png',
+ message: 'Comeback!',
+}
+const message2 = {
+ icon : 'https://nxm.test/wp-content/plugins/notificationx/assets/public/image/flashing-tab/theme-3-icon-2.png',
+ message: null,
+ // message: 'You forgot to purchase!',
+}
 
+// Check if the icon is a gif
+const isGif = (message1?.icon?.match(/\.gif$/) != null);
 
+// Initialize the favloader with the given parameters
 favloader.init({
-    size: 16,
-    radius: 6,
-    thickness: 2,
-    color: '#0F60A8',
-    duration: 5000,
-    // png: "https://nxm.test/wp-content/plugins/notificationx/assets/public/image/icons/verified.svg",
-    // png: settings.ft_icon_1,
-    // gif: "https://nxm.test/wp-content/plugins/notificationx/assets/public/image/icons/blue-face-non-looped.gif",
-    // frame: (context: CanvasRenderingContext2D) => {
-    //     // The size of the emoji is set with the font
-    //     context.font = '14px serif'
-    //     // use these alignment properties for "better" positioning
-    //     context.textAlign = "center";
-    //     context.textBaseline = "middle";
-    //     context.fillText('😜', 8, 8)
-
-    // },
+ size: 16,
+ radius: 6,
+ thickness: 2,
+ color: '#0F60A8',
+ gif: isGif ? message1.icon : null,
 });
 
-window.addEventListener('visibilitychange', (event) => {
-    if (document.visibilityState === "visible") {
-        favloader.stop();
-        clear();
-    } else {
-        // favloader.start();
-        setTimeout(() => {
-            interval_id = interval.set(changeTitle, delayBetween);
-        }, initialDelay);
-    }
-});
+// Declare variables for the toggle and interval states
+let toggle = false;
+let intervalId = null;
+let timeoutId = null;
 
-const changeTitle = () => {
-    if(document.title !== settings.ft_message_1){
-        document.title = settings.ft_message_1;
-        favloader.animatePng(settings.ft_icon_1)
-    }
-    else{
-        document.title = settings.ft_message_2;
-        favloader.animatePng(settings.ft_icon_2)
-    }
+// Save the original title of the document
+const originalTitle = window.document.title;
+
+// Define a function to change the title based on the toggle state and the message
+const changeTitle = (message) => {
+ if(message) {
+   document.title = message;
+ }
 }
 
+// Define a function to animate the icon based on the toggle state and the icon url
+const animateIcon = (icon) => {
+ if(!isGif && icon) {
+   favloader.animatePng(icon);
+ }
+}
+
+// Define a function to switch between the messages and icons based on the toggle state
+const switchMessageAndIcon = () => {
+ toggle = !toggle;
+ if(toggle){
+   // Use message1
+   changeTitle(message1.message);
+   animateIcon(message1.icon);
+ }
+ else{
+   // Use message2
+   changeTitle(message2.message);
+   animateIcon(message2.icon);
+ }
+}
+
+// Define a function to clear the title and icon and stop the intervals
 const clear = () => {
-    document.title = ogTitle;
-    if (interval_id) {
-        interval.clear(interval_id);
-        interval_id = null;
-    }
+ document.title = originalTitle;
+ favloader.stop();
+ if (intervalId) {
+   interval.clear(intervalId);
+   intervalId = null;
+ }
+ if (timeoutId) {
+   clearTimeout(timeoutId);
+   timeoutId = null;
+ }
 }
-// window.addEventListener('blur', () => {
-//     // Tab is out of focus
-//     console.log('Tab is blurred');
 
+// Add an event listener for the visibility change of the document
+window.addEventListener('visibilitychange', (event) => {
+ clear();
+ if (document.visibilityState !== "visible") {
+   // Start the favloader if it is a gif
+   if(isGif){
+     favloader.start();
+   }
 
+   // Set a timeout to start switching between the messages and icons after a delay
+   timeoutId = setTimeout(() => {
+     intervalId = interval.set(switchMessageAndIcon, delayBetween);
+   }, initialDelay);
 
-
-// });
+ }
+});
