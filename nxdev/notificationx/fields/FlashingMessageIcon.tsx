35a330ff77b8c9fa0ad555ec03123c694f0d8a97
Kazi Sourav <khmsourav@gmail.com>
2023-06-04 12:59:42 +0600

    Flashing Tab Frontend work updated

--- nxdev/notificationx/fields/FlashingMessageIcon.tsx
+++ nxdev/notificationx/fields/FlashingMessageIcon.tsx
@@ -1,12 +1,64 @@
 import React, { useEffect, useRef, useState } from "react";
+import { MediaUpload } from '@wordpress/media-utils';
 import { useBuilderContext, withLabel, GenericInput as Input } from "quickbuilder";
+import Tooltip from 'react-power-tooltip';
+import data from '@emoji-mart/data';
+import Picker from '@emoji-mart/react';
+import emojiIconImg from '../../../assets/images/happiness.png'
+import classNames from "classnames";
 
 const FlashingMessageIcon = (props) => {
     const builderContext = useBuilderContext();
     const [value, setValue] = useState(props.value || {});
 
+    // useEffect(() => {
+      
+    // if(!value.icon) {
+    //     setValue({...value, 'icon': props.options[0].icon})
+    // }
+      
+    // }, [])
+    
+    const [show, setShow] = useState(true)
+    const [showEmoji, setShowEmoji] = useState(false)
+
+    const imageClick = () => {
+        setShow(!show)
+    }
+    const emojiClick = () => {
+        setShowEmoji(!showEmoji)
+    }
+
+
+    const changeIcon = (option) => {
+        const _value = {...value, 'icon': option.icon}
+        setShow(false)
+        setValue(_value);
+        props.onChange({
+            target: {
+                type: "advanced-template",
+                value: _value,
+                name: props.name,
+            },
+        });
+    }
+
+    const setImageData = (option) => {
+        const _value = {...value, 'image': option}
+        setShow(false)
+        setValue(_value);
+        props.onChange({
+            target: {
+                type: "advanced-template",
+                value: _value,
+                name: props.name,
+            },
+        });
+    }
+
     const onTextUpdate = (event) => {
         const _value = {...value, 'message': event.val}
+        setValue(_value);
         props.onChange({
             target: {
                 type: "advanced-template",
@@ -18,8 +70,61 @@ const FlashingMessageIcon = (props) => {
 
     return (
         <>
-            <h1>Hello</h1>
+            <span> 
+                <img src={value.icon} alt="iconImg" onClick={imageClick} />
+                <Tooltip show={show} position="right center" arrowAlign="start">
+                    {props.options.map((option,index)=> {
+                        return (
+                            <span onClick={()=>changeIcon(option)}>
+                                <img src={option.icon} alt={option.label} />    
+                            </span>
+                        )
+                    })}
+                    <span>
+                        <img src={emojiIconImg} alt="iconImg" onClick={emojiClick} />
+                        <Tooltip show={showEmoji} position="bottom center" arrowAlign="start">
+                            <span><Picker data={data} /></span>
+                        </Tooltip>
+                    </span>
+                    <span>
+                    <MediaUpload
+                    onSelect={(media) => {
+                        setImageData({
+                            id: media.id,
+                            title: media.title,
+                            url: media.url
+                        });
+                    }}
+                    multiple={false}
+                    allowedTypes={['image']}
+                    value={value?.image}
+                    render={({ open }) => {
+                        return <>
+                            {/* {
+                                imageData != null &&
+                                <button className="wprf-btn wprf-image-remove-btn" onClick={() => setImageData(null)}>
+                                    {props?.remove || 'Remove'}
+                                </button>
+                            } */}
+                            <img src={emojiIconImg}
+                                className={classNames("wprf-btn wprf-image-upload-btn",{
+                                    'uploaded-item': value?.image != null,
+                                })}
+                                onClick={open}
+                                
+
+                            >
+
+                            </img>
+                        </>
+                    }}
+                />
+                    </span>
+                </Tooltip>
+                
+            </span>
             <Input type="text" onChange={onTextUpdate} />
+            
         </>
     );
 };
