472a49f3b1f662a100de5d54473c984d2c098c53
Md. Alimuzzaman Alim <alimuzzamanalim@gmail.com>
2023-09-11 13:24:24 +0600

    Revert Video.php

--- includes/Types/Video.php
+++ includes/Types/Video.php
@@ -42,6 +42,16 @@ class Video extends Types {
     public function __construct() {
         $this->id    = 'video';
         $this->title = __('Video', 'notificationx');
+        $this->popup = [
+            "denyButtonText" => __("<a href='https://notificationx.com/docs/youtube-video-activities-popups/' target='_blank'>More Info</a>", "notificationx"),
+            "confirmButtonText" => __("<a href='https://notificationx.com/#pricing' target='_blank'>Upgrade to PRO</a>", "notificationx"),
+            "html"=> __('
+                <span>NotificationX will help you increasing engagement of your YouTube channel and gaining more credibility.</span>
+                <iframe id="email_subscription_video" type="text/html" allowfullscreen width="450" height="235"
+                src="https://www.youtube.com/embed/gJLQTpumZS4?si=ErcLlvH7V5rnbCaA">
+                </iframe>
+            ', 'notificationx')
+        ];
         parent::__construct();
     }
 
