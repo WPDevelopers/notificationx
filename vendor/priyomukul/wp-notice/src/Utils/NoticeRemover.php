5f1bd7fe29684511d62086e425cc7b08bc92b1b0
Priyo Mukul <17237437+priyomukul@users.noreply.github.com>
2023-11-19 17:10:24 +0600

    Composer Package Updated

--- vendor/priyomukul/wp-notice/src/Utils/NoticeRemover.php
+++ vendor/priyomukul/wp-notice/src/Utils/NoticeRemover.php
@@ -15,11 +15,16 @@ class NoticeRemover {
 		return self::$instance;
 	}
 
-	public function __construct( $version = '1.0.0' ) {
-		add_action( 'init', function () use ( $version ) {
+	public function __construct( $version = '1.0.0', $instanceOf = null ) {
+		add_action( 'init', function () use ( $version, $instanceOf ) {
 			global $wp_filter;
+
+			if ( $instanceOf === null ) {
+				$instanceOf = Notices::class;
+			}
+
 			foreach ( $wp_filter['admin_notices']->callbacks[10] as $callback ) {
-				if ( is_array( $callback['function'] ) && $callback['function'][0] instanceof Notices ) {
+				if ( is_array( $callback['function'] ) && $callback['function'][0] instanceof $instanceOf ) {
 					$notice = $callback['function'][0];
 
 					if ( $notice->version === $version ) {
