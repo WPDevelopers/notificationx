9400bae6f2108eedb24afcee96d03ab7c0b4edf5
Fuad Ragib <ragibfuad@gmail.com>
2024-01-10 11:48:32 +0600

    Fixed source img issue in email subscription type

--- includes/Extensions/ConvertKit/ConvertKit.php
+++ includes/Extensions/ConvertKit/ConvertKit.php
@@ -24,7 +24,7 @@ class ConvertKit extends Extension {
 
     public $priority        = 10;
     public $id              = 'convertkit';
-    public $img             = '';
+    public $img             = NOTIFICATIONX_ADMIN_URL . 'images/extensions/sources/convertkit.png';
     public $doc_link        = 'https://notificationx.com/docs/contact-form-submission-alert/';
     public $types           = 'email_subscription';
     public $module          = 'modules_convertkit';
