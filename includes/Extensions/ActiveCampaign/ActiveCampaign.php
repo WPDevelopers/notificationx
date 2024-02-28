cd7fb87dbf8e2802719b3b46549091d4d9bb44e5
Fuad Ragib <ragibfuad@gmail.com>
2024-02-28 16:46:39 +0600

    Removed video link

--- includes/Extensions/ActiveCampaign/ActiveCampaign.php
+++ includes/Extensions/ActiveCampaign/ActiveCampaign.php
@@ -52,13 +52,11 @@ class ActiveCampaign extends Extension {
 
     public function doc(){
         return sprintf(__('<p>Make sure that you have <a target="_blank" href="%1$s">signed in & retrieved API URL & API key from ActiveCampaign account</a> to use its campaign & email subscriptions data. For further assistance, check out our step by step <a target="_blank" href="%2$s">documentation</a>.</p>
-		<p>🎦 <a target="_blank" href="%3$s">Watch video tutorial</a> to learn quickly</p>
-		<p>👉 NotificationX <a target="_blank" href="%4$s">Integration with ActiveCampaign</a></p>
+		<p>👉 NotificationX <a target="_blank" href="%3$s">Integration with ActiveCampaign</a></p>
 		<p><strong>Recommended Blogs:</strong></p>
-		<p>🔥 Boosting Engagement with <a target="_blank" href="%5$s">ActiveCampaign Email Subscription Alerts</a> via NotificationX</p>', 'notificationx'),
+		<p>🔥 Boosting Engagement with <a target="_blank" href="%4$s">ActiveCampaign Email Subscription Alerts</a> via NotificationX</p>', 'notificationx'),
         'https://help.activecampaign.com/hc/en-us/articles/207317590-Getting-started-with-the-API#getting-started-with-the-api-0-0',
         'https://notificationx.com/docs/activecampaign-email-subscription-alert/',
-        'https://youtu.be/WvX8feM5DBw',
         'https://notificationx.com/integrations/activecampaign/',
         'https://notificationx.com/blog/activecampaign-email-subscription-alerts/'
         );
