d1fc50afb4641fe576be4fbc11e84c65e6343fae
MD Shuvo <66669067+shuvo7670@users.noreply.github.com>
2024-05-20 16:59:44 +0600

    npm build

--- assets/admin/js/admin.asset.php
+++ assets/admin/js/admin.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('lodash', 'moment', 'react', 'react-dom', 'wp-api-fetch', 'wp-components', 'wp-data', 'wp-date', 'wp-element', 'wp-escape-html', 'wp-hooks', 'wp-i18n', 'wp-media-utils'), 'version' => '885c3288fa57e2072b90');
+<?php return array('dependencies' => array('lodash', 'moment', 'react', 'react-dom', 'wp-api-fetch', 'wp-components', 'wp-data', 'wp-date', 'wp-element', 'wp-escape-html', 'wp-hooks', 'wp-i18n', 'wp-media-utils'), 'version' => '15dab3a3bf1406b8e202');
