4e02c74b1588c6228505cef59ba0c55badb36d6f
Kazi Sourav <32020708+khmsourav@users.noreply.github.com>
2024-07-01 20:00:51 +0600

    new admin markup related work

--- nxdev/notificationx/admin/NewAdmin.tsx
+++ nxdev/notificationx/admin/NewAdmin.tsx
@@ -10,6 +10,7 @@ const NewAdmin = () => {
           <img src="/wp-content/plugins/notificationx/assets/admin/images/new-img/add.svg" alt="icon" />
         </a>
       </div>
+
       <div className="nx-admin-content-wrapper nx-started">
         <div className='nx-started-wrapper'>
           <div className='nx-video-widget'>
@@ -25,6 +26,117 @@ const NewAdmin = () => {
               Read Starter Guide
               <img src="/wp-content/plugins/notificationx/assets/admin/images/new-img/link.svg" alt="icon" />
             </a>
+          </div>
+        </div>
+      </div>
+
+      <div className='nx-analytics-wrapper'>
+        <div className='nx-analytics-content-wrapper'>
+          <img src="/wp-content/plugins/notificationx/assets/admin/images/analytics/views-icon.png" alt="icon" />
+          <div className='analytics-counter'>
+            <span className="nx-counter-label">Total Views</span>
+            <h3 className="nx-counter-number">656</h3>
+          </div>
+        </div>
+        <div className='nx-analytics-content-wrapper'>
+          <img src="/wp-content/plugins/notificationx/assets/admin/images/analytics/clicks-icon.png" alt="icon" />
+          <div className='analytics-counter'>
+            <span className="nx-counter-label">Total Clicks</span>
+            <h3 className="nx-counter-number">65,753</h3>
+          </div>
+        </div>
+        <div className='nx-analytics-content-wrapper'>
+          <div>
+            <img src="/wp-content/plugins/notificationx/assets/admin/images/analytics/ctr-icon.png" alt="icon" />
+          </div>
+          <div className='analytics-counter'>
+            <span className="nx-counter-label">Click-Through-Rate</span>
+            <h3 className="nx-counter-number">1.6544</h3>
+          </div>
+        </div>
+      </div>
+
+      <div className='nx-admin-content-wrapper nx-notifications-wrapper'>
+        <div className='nx-integrations-details nx-content-details'>
+          <h4>Integrations</h4>
+          <button className='nx-primary-btn'>View all Notifiation</button>
+        </div>
+        <div className='nx-notifications-details'>
+          <div className='notifications-list-header'>
+            <span className='th'>NotificationX Title</span>
+            <span className='th'>Preview</span>
+            <span className='th'>Status</span>
+            <span className='th'>Type</span>
+            <span className='th'>Stats</span>
+            <span className='th'>Date</span>
+            <span className='th'>Action</span>
+          </div>
+          <div className='notifications-list-body'>
+            <div className='notifications-list-wrapper'>
+              <div className='notifications-list-items'>
+                <span className='td'>NotificationX - Sales Notification - June 9, 2024</span>
+                <span className='td'><img src="" alt="theme img" /></span>
+                <span className='td'>
+                  <label className="toggle-wrap">
+                    <input type="checkbox" />
+                    <span className="slider"></span>
+                  </label>
+                </span>
+                <span className='td'>Sales Notification</span>
+                <span className='td'>10 views</span>
+                <span className='td'>
+                  <span className='td--ex'>Published</span>
+                  June 9, 2024 6:07 am
+                </span>
+                <span className='td'>
+                  <img src="/wp-content/plugins/notificationx/assets/admin/images/new-img/action-btn.svg" alt="icon" />
+                </span>
+              </div>
+              <div className='notifications-list-items'>
+                <span className='td'>NotificationX - Sales Notification - June 9, 2024</span>
+                <span className='td'><img src="" alt="theme img" /></span>
+                <span className='td'>
+                  <label className="toggle-wrap">
+                    <input type="checkbox" checked />
+                    <span className="slider"></span>
+                  </label>
+                </span>
+                <span className='td'>Sales Notification</span>
+                <span className='td'>10 views</span>
+                <span className='td'>
+                  <span className='td--ex'>Published</span>
+                  June 9, 2024 6:07 am
+                </span>
+                <span className='td'>
+                  <img src="/wp-content/plugins/notificationx/assets/admin/images/new-img/action-btn.svg" alt="icon" />
+                </span>
+              </div>
+              <div className='notifications-list-items'>
+                <span className='td'>NotificationX - Sales Notification - June 9, 2024</span>
+                <span className='td'><img src="" alt="theme img" /></span>
+                <span className='td'>
+                  <label className="toggle-wrap">
+                    <input type="checkbox" />
+                    <span className="slider"></span>
+                  </label>
+                </span>
+                <span className='td'>Sales Notification</span>
+                <span className='td'>10 views</span>
+                <span className='td'>
+                  <span className='td--ex'>Published</span>
+                  June 9, 2024 6:07 am
+                </span>
+                <span className='td'>
+                  <img src="/wp-content/plugins/notificationx/assets/admin/images/new-img/action-btn.svg" alt="icon" />
+                </span>
+              </div>
+            </div>
+            <div className='notifications-not-found nx-content-details'>
+              <img src="/wp-content/plugins/notificationx/assets/admin/images/new-img/not-found.svg" alt="icon" />
+              <h5>NO NOTIFICATIONS ARE FOUND.</h5>
+              <p>Seems like you haven’t created any notification alerts. Hit on "Add New" button to get started</p>
+              <button className='nx-primary-btn'>Add New<img src="/wp-content/plugins/notificationx/assets/admin/images/new-img/add.svg" alt="icon"></img></button>
+            </div>
           </div>
         </div>
       </div>
