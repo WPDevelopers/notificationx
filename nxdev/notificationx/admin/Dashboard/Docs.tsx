e384a21325417fb77edac1d759393eee0eb1b0cb
Kazi Sourav <32020708+khmsourav@users.noreply.github.com>
2024-07-27 19:38:01 +0600

    feedback related work show love section & main button UI

--- nxdev/notificationx/admin/Dashboard/Docs.tsx
+++ nxdev/notificationx/admin/Dashboard/Docs.tsx
@@ -1,9 +1,10 @@
 import React from 'react'
 
-const Docs = ( { props, context } ) => {
+const Docs = ({ props, context }) => {
   return (
     <div className='nx-more-docs-wrapper'>
-        <div className='nx-docs-content-wrapper nx-content-details'>
+      <div className='nx-docs-content-wrapper nx-content-details'>
+        <div className='nx-docs-content-items'>
           <div className='img-wrap'>
             <img src="/wp-content/plugins/notificationx/assets/admin/images/new-img/love.svg" alt="icon" />
           </div>
@@ -14,39 +15,40 @@ const Docs = ( { props, context } ) => {
             <img src="/wp-content/plugins/notificationx/assets/admin/images/new-img/link.svg" alt="icon" />
           </a>
         </div>
-        <div className='nx-docs-content-wrapper nx-content-details'>
-          <div className='img-wrap'>
-            <img src="/wp-content/plugins/notificationx/assets/admin/images/new-img/docs.svg" alt="icon" />
-          </div>
-          <h3>Documentations</h3>
-          <p>Get started by spending some time with the documentation to get familiar with NotificationX. Build awesome websites for you or your clients with ease.</p>
-          <a className='nx-resource-link' href="#">
-            Documentation
-            <img src="/wp-content/plugins/notificationx/assets/admin/images/new-img/link.svg" alt="icon" />
-          </a>
+      </div>
+      <div className='nx-docs-content-wrapper nx-content-details'>
+        <div className='img-wrap'>
+          <img src="/wp-content/plugins/notificationx/assets/admin/images/new-img/docs.svg" alt="icon" />
         </div>
-        <div className='nx-docs-content-wrapper nx-content-details'>
-          <div className='img-wrap'>
-            <img src="/wp-content/plugins/notificationx/assets/admin/images/new-img/help.svg" alt="icon" />
-          </div>
-          <h3>Need Help?</h3>
-          <p>Stuck with something? Get help from live chat or support ticket.</p>
-          <a className='nx-resource-link' href="#">
-            Get Support
-            <img src="/wp-content/plugins/notificationx/assets/admin/images/new-img/link.svg" alt="icon" />
-          </a>
+        <h3>Documentations</h3>
+        <p>Get started by spending some time with the documentation to get familiar with NotificationX. Build awesome websites for you or your clients with ease.</p>
+        <a className='nx-resource-link' href="#">
+          Documentation
+          <img src="/wp-content/plugins/notificationx/assets/admin/images/new-img/link.svg" alt="icon" />
+        </a>
+      </div>
+      <div className='nx-docs-content-wrapper nx-content-details'>
+        <div className='img-wrap'>
+          <img src="/wp-content/plugins/notificationx/assets/admin/images/new-img/help.svg" alt="icon" />
         </div>
-        <div className='nx-docs-content-wrapper nx-content-details'>
-          <div className='img-wrap'>
-            <img src="/wp-content/plugins/notificationx/assets/admin/images/new-img/contribute.svg" alt="icon" />
-          </div>
-          <h3>Contribute to NotificationX</h3>
-          <p>You can contribute to make NotificationX better reporting bugs, creating issues, pull requests at GitHub.</p>
-          <a className='nx-resource-link' href="#">
-            Report A Bug
-            <img src="/wp-content/plugins/notificationx/assets/admin/images/new-img/link.svg" alt="icon" />
-          </a>
+        <h3>Need Help?</h3>
+        <p>Stuck with something? Get help from live chat or support ticket.</p>
+        <a className='nx-resource-link' href="#">
+          Get Support
+          <img src="/wp-content/plugins/notificationx/assets/admin/images/new-img/link.svg" alt="icon" />
+        </a>
+      </div>
+      <div className='nx-docs-content-wrapper nx-content-details'>
+        <div className='img-wrap'>
+          <img src="/wp-content/plugins/notificationx/assets/admin/images/new-img/contribute.svg" alt="icon" />
         </div>
+        <h3>Contribute to NotificationX</h3>
+        <p>You can contribute to make NotificationX better reporting bugs, creating issues, pull requests at GitHub.</p>
+        <a className='nx-resource-link' href="#">
+          Report A Bug
+          <img src="/wp-content/plugins/notificationx/assets/admin/images/new-img/link.svg" alt="icon" />
+        </a>
+      </div>
     </div>
   )
 }
