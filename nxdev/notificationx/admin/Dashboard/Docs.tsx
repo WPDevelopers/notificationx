11f565fa389960b623c9597c63521193fbb000a0
Kazi Sourav <32020708+khmsourav@users.noreply.github.com>
2024-08-13 14:13:03 +0600

    button alignment related issues fixing

--- nxdev/notificationx/admin/Dashboard/Docs.tsx
+++ nxdev/notificationx/admin/Dashboard/Docs.tsx
@@ -6,21 +6,19 @@ const Docs = ({ props, context }) => {
   return (
     <div className='nx-more-docs-wrapper'>
       {
-        FOOTER_DOCS.map( (item) => (
+        FOOTER_DOCS.map((item) => (
           <div className='nx-docs-content-wrapper nx-content-details'>
-            <div className='nx-docs-content-items'>
-              <div className='img-wrap'>
-                <img src={ assetsURL(`/images/new-img/${item?.image}`) } alt="icon" />
-              </div>
-              <h3>{ item?.title }</h3>
-              <p>{ item?.desc }</p>
-              <a className='nx-resource-link' target='_blank' href={item?.button_url}>
-                { item?.button_text }
-                <img src={ assetsURL(`/images/new-img/link.svg`) } alt="icon" />
-              </a>
+            <div className='img-wrap'>
+              <img src={assetsURL(`/images/new-img/${item?.image}`)} alt="icon" />
             </div>
+            <h3>{item?.title}</h3>
+            <p>{item?.desc}</p>
+            <a className='nx-resource-link' target='_blank' href={item?.button_url}>
+              {item?.button_text}
+              <img src={assetsURL(`/images/new-img/link.svg`)} alt="icon" />
+            </a>
           </div>
-        ) )
+        ))
       }
     </div>
   )
