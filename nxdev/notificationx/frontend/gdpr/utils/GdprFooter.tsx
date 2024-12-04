d04b5d6a9d7eaa42b72b8f0de1f59fb33f6763f3
MD Shuvo <66669067+shuvo7670@users.noreply.github.com>
2024-12-04 16:19:43 +0600

    gdpr banner & card related class added

--- nxdev/notificationx/frontend/gdpr/utils/GdprFooter.tsx
+++ nxdev/notificationx/frontend/gdpr/utils/GdprFooter.tsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import NXBranding from '../../themes/helpers/NXBranding'
+import { __ } from '@wordpress/i18n'
 
 const GdprFooter = ({ settings }) => {
     
   return (
     <div className={`nx-gdpr-card-footer ${settings?.gdpr_theme}`}>
-        <p className="nx-gdpr-powered">Powered by</p>
+        <p className="nx-gdpr-powered">{ __('Powered by','notificationx') }</p>
         <NXBranding/>
     </div>
   )
