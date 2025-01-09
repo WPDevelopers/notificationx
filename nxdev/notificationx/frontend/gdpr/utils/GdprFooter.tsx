6c62b879203755a34ffe408c4d205f31c577c46a
Fuad Ragib <ragibfuad@gmail.com>
2025-01-09 14:43:54 +0600

    Added custom css for advanced design

--- nxdev/notificationx/frontend/gdpr/utils/GdprFooter.tsx
+++ nxdev/notificationx/frontend/gdpr/utils/GdprFooter.tsx
@@ -3,9 +3,15 @@ import NXBranding from '../../themes/helpers/NXBranding'
 import { __ } from '@wordpress/i18n'
 
 const GdprFooter = ({ settings }) => {
-    
+
+  let footerBGColor = {};
+  if ( settings?.advance_edit ) {
+    footerBGColor = {
+        backgroundColor: settings?.gdpr_design_ft_bg_color,
+    };
+}
   return (
-    <div className={`nx-gdpr-card-footer ${settings?.gdpr_theme}`}>
+    <div className={`nx-gdpr-card-footer ${settings?.gdpr_theme}`} style={footerBGColor}>
         <p className="nx-gdpr-powered">{ __('Powered by','notificationx') }</p>
         <NXBranding/>
     </div>
