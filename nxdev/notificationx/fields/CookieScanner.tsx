d1addaee4d5ed61e9292c6b6c9b762e4d85d8048
Fuad Ragib <ragibfuad@gmail.com>
2025-03-09 13:09:31 +0600

    UI for scan cookie

--- nxdev/notificationx/fields/CookieScanner.tsx
+++ nxdev/notificationx/fields/CookieScanner.tsx
@@ -1,4 +1,6 @@
 import React, { useState, useEffect } from 'react';
+import scan from './images/scan.png';
+import schedule from './images/coming-soon.png';
 
 const CookieScanner = () => {
   const [scanStatus, setScanStatus] = useState('');
@@ -69,11 +71,31 @@ const CookieScanner = () => {
   }, [scanId]);
 
   return (
-    <div>
-      <button onClick={handleScan} disabled={isScanning}>
-        {isScanning ? 'Scanning...' : 'Scan Now'}
-      </button>
-      <p>{scanStatus}</p>
+    <div id='cookie-scanner'>
+      <div className='scan-controls'>
+        <div>
+          <div className='scan-img'>
+            <img src={scan} alt={'Scan cookie'} />
+          </div>
+          <div className='scan-actions'>
+            <p>No scan history found</p>
+            <h2>Start your first scan now</h2>
+            <div className='scan-action-btns'>
+              <button onClick={handleScan} disabled={isScanning} className='primary'>
+                {isScanning ? 'Scanning...' : 'Scan Now'}
+              </button>
+              <button onClick={handleScan} disabled={isScanning} className='secondary'>
+                History
+              </button>
+            </div>
+            <p>{scanStatus}</p>
+          </div>
+        </div>
+        <p><span>0 of 5</span> free scans used</p>
+      </div>
+      <div className='scan-schedule'>
+        <img src={schedule} alt={'Comming soon'} />
+      </div>
     </div>
   );
 };
