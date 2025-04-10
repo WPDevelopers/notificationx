2bf536c947a24cc8bfb531dbd0401392f24efe27
MD Shuvo <66669067+shuvo7670@users.noreply.github.com>
2025-04-10 15:26:39 +0600

    analytics card link issue fixed

--- nxdev/notificationx/admin/Analytics/AnalyticsHeader.tsx
+++ nxdev/notificationx/admin/Analytics/AnalyticsHeader.tsx
@@ -9,23 +9,26 @@ const AnalyticsHeader = ({ assetsURL }) => {
         return <></>;
     }
     const analytics = builderContext?.state?.analytics;
-    
+    const is_pro    = builderContext?.is_pro_active;
     return (
         <div className="nx-analytics-counter-wrapper">
             <AnalyticsCard
                 type="views"
                 icon={`${assetsURL.admin}images/analytics/views-icon.png`}
                 title={__("Total Views", 'notificationx')} count={analytics?.totalViews} url=""
+                is_pro={is_pro}
             />
             <AnalyticsCard
                 type="clicks"
                 icon={`${assetsURL.admin}images/analytics/clicks-icon.png`}
                 title={__("Total Clicks", 'notificationx')} count={analytics?.totalClicks} url=""
+                is_pro={is_pro}
             />
             <AnalyticsCard
                 type="ctr"
                 icon={`${assetsURL.admin}images/analytics/ctr-icon.png`}
                 title={__("Click-Through-Rate", 'notificationx')} count={analytics?.totalCtr + "%"} url=""
+                is_pro={is_pro}
             />
         </div>
     )
