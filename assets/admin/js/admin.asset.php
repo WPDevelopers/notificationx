8d4cd8936b6964837dd5ae20d650816e6fac16be
shuvo7670 <mdshakibul.shuvo@gmail.com>
2025-05-07 13:07:13 +0600

    version bump & npm build

--- assets/admin/js/admin.asset.php
+++ assets/admin/js/admin.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('lodash', 'moment', 'react', 'react-dom', 'wp-api-fetch', 'wp-components', 'wp-compose', 'wp-data', 'wp-date', 'wp-element', 'wp-escape-html', 'wp-hooks', 'wp-i18n', 'wp-media-utils', 'wp-polyfill'), 'version' => 'dee0dfdedff505838a1c');
+<?php return array('dependencies' => array('lodash', 'moment', 'react', 'react-dom', 'wp-api-fetch', 'wp-components', 'wp-compose', 'wp-data', 'wp-date', 'wp-element', 'wp-escape-html', 'wp-hooks', 'wp-i18n', 'wp-media-utils', 'wp-polyfill'), 'version' => 'a0dc11631d77a665f3d7');
