c2d484602194b3d2b94d82af720a6b2d10e9efdb
shuvo7670 <mdshakibul.shuvo@gmail.com>
2025-05-19 12:57:21 +0600

    Update PressbarAdminPreview.jsx

--- nxdev/notificationx/fields/helpers/PressbarAdminPreview.jsx
+++ nxdev/notificationx/fields/helpers/PressbarAdminPreview.jsx
@@ -1,10 +1,14 @@
 import { __ } from "@wordpress/i18n";
 import classNames from "classnames";
 import moment from "moment";
-import React, { useEffect, useRef, useState } from "react";
+import React, { Fragment, useEffect, useRef, useState } from "react";
 import cookie from "js-cookie"; // Assuming this is used
 import BarCoupon from "../../frontend/core/helper/BarCoupon";
 import PreviewButton from './PreviewButton';
+import { ReactComponent as DesktopIcon } from "../../icons/responsive/desktop.svg";
+import { ReactComponent as TabletIcon } from "../../icons/responsive/tablet.svg";
+import { ReactComponent as MobileIcon } from "../../icons/responsive/mobile.svg";
+
 
 const getUnixTime = (value) => moment.utc(value).unix() * 1000;
 
@@ -57,6 +61,7 @@ const PressbarAdminPreview = ({ position, nxBar, dispatch }) => {
     const [isTimeBetween, setIsTimeBetween] = useState(false);
     const [styles, setStyles] = useState({});
     const countdownRef = useRef(null);
+    const [previewType, setPreviewType] = useState("desktop");
 
     const getTime = () => {
         const currentTime = Date.now();
@@ -191,106 +196,132 @@ const PressbarAdminPreview = ({ position, nxBar, dispatch }) => {
     const { link, link_text } = resolveLinkInfo(settings, data);    
 
     return (
-        <div
-            id={`nx-bar-${settings.nx_id}`}
-            className={classNames(
-                "nx-bar",
-                settings.themes,
-                `nx-bar-${settings.nx_id}`,
-                'nx-bar-section-preview',
-                {
-                    "nx-position-top": settings?.position === "top",
-                    "nx-position-bottom": settings?.position === "bottom",
-                    [`nx-close-${settings?.bar_close_position}`]: settings?.bar_close_position,
-                    "nx-sticky-bar": settings?.sticky_bar,
-                }
-            )}
-            style={{...styles?.componentCSS}}
-        >
-            <div className="nx-bar-inner">
-                <div className="nx-bar-content-wrap">
-                    {settings?.enable_countdown && (
-                        <div className="nx-countdown-wrapper">
-                            {!timeConfig.expired && settings?.countdown_text && (
-                                <div className="nx-countdown-text">{__(settings.countdown_text, "notificationx")}</div>
+        <Fragment>
+            <div className="nx-bar-responsive">
+                <div class="nx-admin-modal-head">
+                    <button 
+                        class={`nx-admin-modal-preview-button ${previewType == "desktop" ? "active" : ""}`}
+                        onClick={() => setPreviewType("desktop")}
+                    >
+                         <DesktopIcon style={{ width: 20 }} />
+                    </button>
+                    <button
+                        onClick={() => setPreviewType("tablet")}
+                        class={`nx-admin-modal-preview-button ${previewType == "tablet" ? "active" : ""}`}
+                    >
+                         <TabletIcon style={{ width: 20 }} />
+                    </button>
+                    <button
+                        onClick={() => setPreviewType("phone")}
+                        class={`nx-admin-modal-preview-button ${previewType == "phone" ? "active" : ""}`}
+                    >
+                         <MobileIcon style={{ width: 20 }} />
+                    </button>
+                </div>
+            </div>
+            <div
+                id={`nx-bar-${settings.nx_id}`}
+                className={classNames(
+                    "nx-bar",
+                    settings.themes,
+                    `nx-bar-${settings.nx_id}`,
+                    'nx-bar-section-preview',
+                    `nx-bar-${previewType}`,
+                    {
+                        "nx-position-top": settings?.position === "top",
+                        "nx-position-bottom": settings?.position === "bottom",
+                        [`nx-close-${settings?.bar_close_position}`]: settings?.bar_close_position,
+                        "nx-sticky-bar": settings?.sticky_bar,
+                    }
+                )}
+                style={{...styles?.componentCSS}}
+            >
+                <div className="nx-bar-inner">
+                    <div className="nx-bar-content-wrap">
+                        {settings?.enable_countdown && (
+                            <div className="nx-countdown-wrapper">
+                                {!timeConfig.expired && settings?.countdown_text && (
+                                    <div className="nx-countdown-text">{__(settings.countdown_text, "notificationx")}</div>
+                                )}
+                                {!timeConfig.expired && settings?.countdown_text && (
+                                    <div className="nx-countdown-text">
+                                        {__(settings.countdown_text, "notificationx")}
+                                    </div>
+                                )}
+                                <div className="nx-countdown" style={styles.counterCSS}>
+                                    {( (!timeConfig.expired && isTimeBetween) || (!timeConfig.expired && settings?.evergreen_timer)) ? (
+                                        ['days', 'hours', 'minutes', 'seconds'].map((key) => (
+                                            <div key={key} className="nx-time-section" style={styles.counterCSS}>
+                                                <span className={`nx-${key}`}>{timeConfig[key]}</span>
+                                                <span className="nx-countdown-time-text">{__(key.charAt(0).toUpperCase() + key.slice(1), "notificationx")}</span>
+                                            </div>
+                                        ))
+                                    ) : (
+                                        <span className={classNames("nx-expired-text", { "nx-countdown-expired": timeConfig.expired })} style={styles.counterCSS}>
+                                            {timeConfig.expired && __(settings?.countdown_expired_text, "notificationx")}
+                                        </span>
+                                    )}
+                                </div>
+                            </div>
+                        )}
+                        <div className="nx-inner-content-wrapper">
+                            {( settings?.bar_content_type == 'static' && hasContent) && (
+                                <div className="nx-bar-content" dangerouslySetInnerHTML={{ __html: settings?.press_content }}></div>
                             )}
-                            {!timeConfig.expired && settings?.countdown_text && (
-                                <div className="nx-countdown-text">
-                                    {__(settings.countdown_text, "notificationx")}
+                            {settings?.bar_content_type === 'sliding' && slidingContent.length > 0 && (
+                                <div className={classNames("nx-bar-content nx-bar-slide-wrapper", `slide-direction-${direction}`)}>
+                                    {slidingContent.map((item, index) => {
+                                    const isActive = index === currentSlide;
+                                    const isPrevious = (index === currentSlide - 1) || (currentSlide === 0 && index === slidingContent.length - 1);
+        
+                                    return (
+                                        <div
+                                            key={index}
+                                            className={classNames("nx-bar-slide", {
+                                                'active': isActive,
+                                                'previous': isPrevious,
+                                                'left-exit': isPrevious,
+                                                'right-exit': isPrevious
+                                            })}
+                                            style={{
+                                                transition: `all ${transitionSpeed}ms ease-in-out`
+                                            }}
+                                            dangerouslySetInnerHTML={{ __html: item.title }}
+                                        />
+                                    );
+                                    })}
                                 </div>
                             )}
-                            <div className="nx-countdown" style={styles.counterCSS}>
-                                {( (!timeConfig.expired && isTimeBetween) || (!timeConfig.expired && settings?.evergreen_timer)) ? (
-                                    ['days', 'hours', 'minutes', 'seconds'].map((key) => (
-                                        <div key={key} className="nx-time-section" style={styles.counterCSS}>
-                                            <span className={`nx-${key}`}>{timeConfig[key]}</span>
-                                            <span className="nx-countdown-time-text">{__(key.charAt(0).toUpperCase() + key.slice(1), "notificationx")}</span>
-                                        </div>
-                                    ))
-                                ) : (
-                                    <span className={classNames("nx-expired-text", { "nx-countdown-expired": timeConfig.expired })} style={styles.counterCSS}>
-                                        {timeConfig.expired && __(settings?.countdown_expired_text, "notificationx")}
-                                    </span>
-                                )}
-                            </div>
-                        </div>
-                    )}
-                    <div className="nx-inner-content-wrapper">
-                        {( settings?.bar_content_type == 'static' && hasContent) && (
-                            <div className="nx-bar-content" dangerouslySetInnerHTML={{ __html: settings?.press_content }}></div>
-                        )}
-                        {settings?.bar_content_type === 'sliding' && slidingContent.length > 0 && (
-                            <div className={classNames("nx-bar-content nx-bar-slide-wrapper", `slide-direction-${direction}`)}>
-                                {slidingContent.map((item, index) => {
-                                const isActive = index === currentSlide;
-                                const isPrevious = (index === currentSlide - 1) || (currentSlide === 0 && index === slidingContent.length - 1);
-    
-                                return (
-                                    <div
-                                        key={index}
-                                        className={classNames("nx-bar-slide", {
-                                            'active': isActive,
-                                            'previous': isPrevious,
-                                            'left-exit': isPrevious,
-                                            'right-exit': isPrevious
-                                        })}
-                                        style={{
-                                            transition: `all ${transitionSpeed}ms ease-in-out`
-                                        }}
-                                        dangerouslySetInnerHTML={{ __html: item.title }}
-                                    />
-                                );
-                                })}
-                            </div>
-                        )}
-    
-                        {!hasContent && (
-                            <div className="nx-bar-content">
-                                {__(
-                                    "You should setup NX Bar properly",
-                                    "notificationx"
-                                )}
-                            </div>
-                        )}
+        
+                            {!hasContent && (
+                                <div className="nx-bar-content">
+                                    {__(
+                                        "You should setup NX Bar properly",
+                                        "notificationx"
+                                    )}
+                                </div>
+                            )}
 
-                        {(settings?.button_url && settings?.button_text) && (
-                            <PreviewButton
-                                style={styles?.buttonCSS}
-                                className="nx-bar-button"
-                                href={settings?.button_url}
-                                config={settings}
-                            >
-                                {settings?.button_text}
-                            </PreviewButton>
-                        )}
-    
-                        {settings?.coupon_text && settings?.enable_coupon && (
-                            <BarCoupon settings={settings} />
-                        )}
+                            {(settings?.button_url && settings?.button_text) && (
+                                <PreviewButton
+                                    style={styles?.buttonCSS}
+                                    className="nx-bar-button"
+                                    href={settings?.button_url}
+                                    config={settings}
+                                >
+                                    {settings?.button_text}
+                                </PreviewButton>
+                            )}
+        
+                            {settings?.coupon_text && settings?.enable_coupon && (
+                                <BarCoupon settings={settings} />
+                            )}
+                        </div>
                     </div>
                 </div>
             </div>
-        </div>
+        </Fragment>
+        
     );
 };
 
