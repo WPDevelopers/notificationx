0f1c171f6472ff2a1b886c360692dd1f1a22d12c
shuvo7670 <mdshakibul.shuvo@gmail.com>
2025-10-08 17:09:32 +0600

    style related issue fixing.

--- assets/admin/js/admin.asset.php
+++ assets/admin/js/admin.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('lodash', 'moment', 'react', 'react-dom', 'wp-api-fetch', 'wp-components', 'wp-compose', 'wp-data', 'wp-date', 'wp-element', 'wp-escape-html', 'wp-hooks', 'wp-i18n', 'wp-media-utils', 'wp-polyfill'), 'version' => 'd6164c99e4cd823bc2b8');
+<?php return array('dependencies' => array('lodash', 'moment', 'react', 'react-dom', 'wp-api-fetch', 'wp-components', 'wp-compose', 'wp-data', 'wp-date', 'wp-element', 'wp-escape-html', 'wp-hooks', 'wp-i18n', 'wp-media-utils', 'wp-polyfill'), 'version' => 'ae2fb3c1f6db62c385fa');
