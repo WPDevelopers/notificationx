f8a609f7df55684590db0c286308b8fa9366dd4e
shuvo7670 <mdshakibul.shuvo@gmail.com>
2025-11-06 16:09:28 +0600

    Update notificationx.php

--- notificationx.php
+++ notificationx.php
@@ -81,7 +81,11 @@ function nx_free_compatibility_notice() {
     ?>
         <div class="notice notice-warning is-dismissible">
             <p>
-            <?php echo sprintf( __( "<strong>Recommended: </strong> Seems like you haven't updated the NotificationX Pro version. Please make sure to update NotificationX Pro plugin from <a href='%s'><strong>wp-admin -> Plugins</strong></a>.", 'notificationx' ), esc_url( admin_url( 'plugins.php' ) ) ); ?></p>
+            <?php 
+                /* translators: %s: URL to the WordPress Plugins page (wp-admin -> Plugins) */
+                echo sprintf( __( "<strong>Recommended: </strong> Seems like you haven't updated the NotificationX Pro version. Please make sure to update NotificationX Pro plugin from <a href='%s'><strong>wp-admin -> Plugins</strong></a>.", 'notificationx' ), esc_url( admin_url( 'plugins.php' ) ) ); 
+                ?>
+            </p>
         </div>
     <?php
 }
