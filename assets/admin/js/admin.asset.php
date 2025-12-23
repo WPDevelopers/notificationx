dc9fff797e19eeb22d80da7915612953fa9b0cf3
shuvo7670 <mdshakibul.shuvo@gmail.com>
2025-12-23 12:26:11 +0600

    npm build

--- assets/admin/js/admin.asset.php
+++ assets/admin/js/admin.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('lodash', 'moment', 'react', 'react-dom', 'wp-api-fetch', 'wp-components', 'wp-compose', 'wp-data', 'wp-date', 'wp-element', 'wp-escape-html', 'wp-hooks', 'wp-i18n', 'wp-media-utils', 'wp-polyfill'), 'version' => 'b43953a018df8cc7a6db');
+<?php return array('dependencies' => array('lodash', 'moment', 'react', 'react-dom', 'wp-api-fetch', 'wp-components', 'wp-compose', 'wp-data', 'wp-date', 'wp-element', 'wp-escape-html', 'wp-hooks', 'wp-i18n', 'wp-media-utils', 'wp-polyfill'), 'version' => 'f4e5157389c1057aeb1c');
