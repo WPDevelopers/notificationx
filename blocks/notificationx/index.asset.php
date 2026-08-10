e3c750e934c5b5dff321366e7bf990df41623714
Fuad Ragib <ragibfuad@gmail.com>
2026-08-10 18:28:09 +0600

    FIxed build related issues

--- blocks/notificationx/index.asset.php
+++ blocks/notificationx/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-hooks', 'wp-i18n', 'wp-server-side-render'), 'version' => '5d5d06f6a083656f9459');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-hooks', 'wp-i18n', 'wp-server-side-render'), 'version' => '086b6a8940b6618134b9');
